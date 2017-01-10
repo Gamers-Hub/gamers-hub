01b442b80f0d4c7e08ca1a3c62f850973258bb2c
Sujay Kundu <sujaykundu@localhost.localdomain>
2017-01-10 20:48:43 +0530

    Reinitiate App

---
+++ app/packages/nova-base-components/lib/common/Footer.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { FormattedMessage } from 'react-intl';
+
+const Footer = props => {
+  return (
+    <div className="footer">All Rights Reserved !</div>
+  )
+}
+
+Footer.displayName = "Footer";
+
+module.exports = Footer;
