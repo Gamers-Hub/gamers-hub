24d86aa6bcf9bcb7bb7b1545f62d5ef43f51dbcc
Sujay Kundu <sujaykundu@localhost.localdomain>
2017-01-16 22:37:31 +0530

    Add Logo

--- app/packages/gamershub/lib/components.js
+++ app/packages/gamershub/lib/components.js
@@ -3,7 +3,7 @@ This file centralizes all our custom component overrides.
 */
 
 import Telescope from 'meteor/nova:lib';
-
+import CustomLogo from "./components/CustomLogo.jsx";
 import CustomNewsletter from "./components/CustomNewsletter.jsx";
 import CustomPostsItem from "./components/CustomPostsItem.jsx";
 import CustomHeader from "./components/CustomHeader.jsx";
@@ -12,3 +12,4 @@ import CustomHeader from "./components/CustomHeader.jsx";
 Telescope.components.Newsletter = CustomNewsletter;
 Telescope.components.PostsItem = CustomPostsItem;
 Telescope.components.Header = CustomHeader;
+Telescope.components.Logo = CustomLogo;
--- app/packages/nova-base-components/lib/common/Logo.jsx
+++ app/packages/nova-base-components/lib/common/Logo.jsx
@@ -3,15 +3,21 @@ import { IndexLink } from 'react-router';
 
 const Logo = ({logoUrl, siteTitle}) => {
 
+if (logoUrl) {
     return (
       <h1 className="logo-image ">
         <IndexLink to={{pathname: "/"}}>
-          <img src="https://lh3.googleusercontent.com/-z8pL0sLK8Gw/WHzbjSW8DfI/AAAAAAAAObk/lgRtTGtQiB4zMzBpfN-Gl9Ob2p5TXFJlACEw/h77/logo.png" alt={siteTitle} style={{maxWidth: "100px", maxHeight: "100px"}} />
+          <img src={logoUrl} alt={siteTitle} style={{maxWidth: "100px", maxHeight: "100px"}} />
         </IndexLink>
       </h1>
     )
+  } else {
+    return (
+      <h1 className="logo-text">
+        <IndexLink to={{pathname: "/"}}>{siteTitle}</IndexLink>
+      </h1>
+    )
   }
-
 
 Logo.displayName = "Logo";
 
