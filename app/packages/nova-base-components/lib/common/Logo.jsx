942cc045b0a93e0cde2b41a3bfa4c9c60ccc033c
Sujay Kundu <sujaykundu@localhost.localdomain>
2017-01-16 21:57:29 +0530

    Add Logo

--- app/packages/nova-base-components/lib/common/Logo.jsx
+++ app/packages/nova-base-components/lib/common/Logo.jsx
@@ -2,18 +2,12 @@ import React from 'react';
 import { IndexLink } from 'react-router';
 
 const Logo = ({logoUrl, siteTitle}) => {
-  if (logoUrl) {
+
     return (
       <h1 className="logo-image ">
         <IndexLink to={{pathname: "/"}}>
-          <img src={logoUrl} alt={siteTitle} style={{maxWidth: "100px", maxHeight: "100px"}} />
+          <img src="https://lh3.googleusercontent.com/-z8pL0sLK8Gw/WHzbjSW8DfI/AAAAAAAAObk/lgRtTGtQiB4zMzBpfN-Gl9Ob2p5TXFJlACEw/h77/logo.png" alt={siteTitle} style={{maxWidth: "100px", maxHeight: "100px"}} />
         </IndexLink>
-      </h1>
-    )
-  } else {
-    return (
-      <h1 className="logo-text">
-        <IndexLink to={{pathname: "/"}}>{siteTitle}</IndexLink>
       </h1>
     )
   }
