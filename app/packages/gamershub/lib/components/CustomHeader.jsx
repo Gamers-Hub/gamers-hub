07af9e40da31685e99c668d282940698e4bf020c
Sujay Kundu <sujaykundu777@gmail.com>
2017-09-15 13:31:48 +0530

    Changed Link

--- app/packages/gamershub/lib/components/CustomHeader.jsx
+++ app/packages/gamershub/lib/components/CustomHeader.jsx
@@ -10,7 +10,7 @@ const CustomHeader = (props, {currentUser}) => {
       <header className="header">
 
         <div className="logo">
-             <a href="http://www.gamershub.in">
+             <a href="http://gamershub-in.herokuapp.com">
           <img src="https://lh3.googleusercontent.com/-z8pL0sLK8Gw/WHzbjSW8DfI/AAAAAAAAObk/lgRtTGtQiB4zMzBpfN-Gl9Ob2p5TXFJlACEw/h77/logo.png" class="logoimg" />
          </a>
         </div>
